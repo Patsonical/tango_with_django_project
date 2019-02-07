c75505b352ca7dbb1cbe649ff39c386c9b5fc178
Patryk Kaczmarczyk <2322928k@student.gla.ac.uk>
2019-02-07 15:30:48 +0000

    Added test cookie to index and about

--- rango/views.py
+++ rango/views.py
@@ -7,12 +7,18 @@ from django.core.urlresolvers import reverse
 from django.contrib.auth.decorators import login_required
 
 def index(request):
+    request.session.set_test_cookie()
+
     category_list = Category.objects.order_by("-likes")[:5]
     page_list     = Page.objects.order_by("-views")[:5]
     context_dict  = {"categories": category_list, "pages": page_list}
     return render(request, "rango/index.html", context=context_dict)
 
 def about(request):
+    if request.session.test_cookie_worked():
+        print("TEST COOKIE WORKED!")
+        request.session.delete_test_cookie()
+
     context_dict = {}
     return render(request, "rango/about.html", context=context_dict)
 
